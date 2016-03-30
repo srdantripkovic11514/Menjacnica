a05777c7b7cef12a8216b8298d0126f4bac40f26
srdantripkovic11514 <srdan.tripkovic@gmail.com>
2016-03-30 20:40:10 +0200

    Dodat interfejs sa metodama u okviru nove grane specifikacijainterfejsa

---
+++ Menjacnica/src/interfejs/MenjacnicaInterfejs.java
@@ -0,0 +1,11 @@
+package interfejs;
+
+import java.util.GregorianCalendar;
+
+public interface MenjacnicaInterfejs {
+	void dodajKurs(GregorianCalendar datum);
+
+	void izbrisiKurs(GregorianCalendar datum);
+
+	void nadjiKurs(GregorianCalendar datum);
+}
