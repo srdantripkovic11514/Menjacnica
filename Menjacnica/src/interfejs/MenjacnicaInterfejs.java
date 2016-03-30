94ff875eb7cf0fd43b4286b792a9aa606ca95fb6
srdantripkovic11514 <srdan.tripkovic@gmail.com>
2016-03-30 21:26:01 +0200

    Doradjen interfejs

--- Menjacnica/src/interfejs/MenjacnicaInterfejs.java
+++ Menjacnica/src/interfejs/MenjacnicaInterfejs.java
@@ -2,10 +2,12 @@ package interfejs;
 
 import java.util.GregorianCalendar;
 
+import valute.Valuta;
+
 public interface MenjacnicaInterfejs {
-	void dodajKurs(GregorianCalendar datum);
+	void dodajKurs(GregorianCalendar datum, Valuta valuta, double prodajniKurs, double kupovniKurs, double srednjiKurs);
 
-	void izbrisiKurs(GregorianCalendar datum);
+	void izbrisiKurs(GregorianCalendar datum, Valuta valuta);
 
-	void nadjiKurs(GregorianCalendar datum);
+	void nadjiKurs(GregorianCalendar datum, Valuta valuta);
 }
